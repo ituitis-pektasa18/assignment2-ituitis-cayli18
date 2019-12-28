275ad8e8302a495c008d1f300d2f2fb10cc6d732
ituitis-cayli18 <55956382+ituitis-cayli18@users.noreply.github.com>
2019-12-28 13:59:55 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,17 +1,35 @@
-import os
+from bottle import run, route, request, template, default_app, debug, static_file
+from hashlib import sha256 
 
-from bottle import Bottle
+def create_hash(password):
+	pw_bytestring = password.encode()
+	return sha256(pw_bytestring).hexdigest()
 
+def htmlx(title,body,back):
+	page = """
+	<!doctype html>
+	<html lang="en">
+		<head>
+			<meta charset="utf-8">
+			<title>%s</title>
+		</head>
+		<body>
+			<div>
+				%s <br>
+				%s
+			</div>
+		</body>
+	</html>	
+""" % (title,body,back)
+return page
 
-def home_page():
-    return "Assignment 2"
+hash_password = "fbfdd827b403abd8146ca46ccf07eae5370477f90f6b5be72dcd5e67d1ae6a54"	
+comment_list = []
 
-
-def create_app():
-    app = Bottle()
-    app.route("/", "GET", home_page)
-    return app
-
-
-application = create_app()
-application.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+def index():
+	return static_file('index.html', root="./")
+	
+@route("/getip")
+def get_ip():
+	client_ip = request.environ.get("HTTP_X_FORWARDED_FOR") or request.environ.get("REMOTE_ADDR")
+	print["Your ip is: {}\n".format(client_ip)]
