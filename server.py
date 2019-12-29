f83a6b27e7d691ff22071097c1ebdf2b0a5259f0
ituitis-cayli18 <55956382+ituitis-cayli18@users.noreply.github.com>
2019-12-30 01:37:32 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,35 +1,40 @@
-from bottle import run, route, request, template, default_app, debug, static_file
-from hashlib import sha256 
-
+import os
+from bottle import *
+from hashlib import sha256
+from pathlib import Path
+user_password = "fbfdd827b403abd8146ca46ccf07eae5370477f90f6b5be72dcd5e67d1ae6a54"
 def create_hash(password):
-	pw_bytestring = password.encode()
-	return sha256(pw_bytestring).hexdigest()
+    pw_bytestring = password.encode()
+    return sha256(pw_bytestring).hexdigest()
+ips = []
+def add_ip(a,b):
+    global ips
+    if a in b:
+        a[b] += 1
+    else:
+        a[b] = 1
+def home():
+    new_ip = request.headers.get("X-Forwarded-For", "127.0.0.1")
+    global ips
+    add_ip(new_ip,ips)
+    tableContent ="""
+    <html>
+        <body>  
+            <tr>
+                <td>%s (new_ip) %s </td>
+                <td>%s (ips[new_ip] %s </td>
+            </tr>
+        </body>
+    </html>
+    """
+    return tableContent
+def get_edu():
+    return Path("/index1.html")
+def create_app():
+    app = Bottle()
+    app.route("/", "GET", home)
+    app.route("/login", "GET", get_edu)
 
-def htmlx(title,body,back):
-	page = """
-	<!doctype html>
-	<html lang="en">
-		<head>
-			<meta charset="utf-8">
-			<title>%s</title>
-		</head>
-		<body>
-			<div>
-				%s <br>
-				%s
-			</div>
-		</body>
-	</html>	
-""" % (title,body,back)
-return page
-
-hash_password = "fbfdd827b403abd8146ca46ccf07eae5370477f90f6b5be72dcd5e67d1ae6a54"	
-comment_list = []
-
-def index():
-	return static_file('index.html', root="./")
-	
-@route("/getip")
-def get_ip():
-	client_ip = request.environ.get("HTTP_X_FORWARDED_FOR") or request.environ.get("REMOTE_ADDR")
-	print["Your ip is: {}\n".format(client_ip)]
+    return app
+application = create_app()
+application.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
